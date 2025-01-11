490a656695fda2e62fa59656f70538a7392aa162
Oliver-123-321 <2749371355@qq.com>
2025-01-11 13:27:20 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,4 @@
+document.getElementById('changeTextBtn').addEventListener('click', function() {
+    var textElement = document.getElementById("text");
+    textElement.textContent = "New Text Here!"; 
+});
